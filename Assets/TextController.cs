82227b19e34bd348c5218b5f9ba6da71295e5b8d
Maxime Buttard <maxime.buttard@gmail.com>
2017-04-21 15:09:29 +0200

    adding more and more states

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -6,19 +6,19 @@ public class TextController : MonoBehaviour
 {
 
 	public Text text;
-	private enum States {chavant, banc, affiche, tram, papier_chavant, nord, est, ouest, change_scene};
+	private enum States {chavant_0, banc, affiche, tram, chavant_1, nord, est, ouest, bobine, chavant_2, chavant_3};
 	private States myState;
 
 	// Use this for initialization
 	void Start () {
-		myState = States.chavant;
+		myState = States.chavant_0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		print (myState);
 		
-		if (myState == States.chavant) {
+		if (myState == States.chavant_0) {
 			chavant();
 		} else if (myState == States.banc) {
 			banc();
@@ -26,14 +26,20 @@ public class TextController : MonoBehaviour
 			tram();
 		} else if (myState == States.affiche) {
 			papier();
-		} else if (myState == States.papier_chavant) {
-			papier_chavant();
+		} else if (myState == States.chavant_1) {
+			chavant_1();
 		} else if (myState == States.nord) {
 			nord();
 		} else if (myState == States.est) {
 			est();
 		} else if (myState == States.ouest) {
 			ouest();
+		} else if (myState == States.chavant_2) {
+			chavant_2();
+		} else if (myState == States.chavant_3) {
+			chavant_3();
+		} else if (myState == States.bobine) {
+			bobine();
 		}
 		
 	}
@@ -59,7 +65,7 @@ public class TextController : MonoBehaviour
 					"Appui sur R pour observer autre chose.";
 		
 		if (Input.GetKeyDown (KeyCode.R)) {			
-			myState = States.chavant;						
+			myState = States.chavant_0;						
 		} 			
 	}
 	
@@ -69,7 +75,7 @@ public class TextController : MonoBehaviour
 					"Appui sur R pour observer autre chose.";
 		
 		if (Input.GetKeyDown (KeyCode.R)) {			
-			myState = States.chavant;						
+			myState = States.chavant_0;						
 		} 			
 	}
 	
@@ -79,13 +85,13 @@ public class TextController : MonoBehaviour
 					"Appui sur L pour Lire le papier, ou sur R pour observer autre chose.";
 		
 		if (Input.GetKeyDown (KeyCode.R)) {			
-			myState = States.chavant;						
+			myState = States.chavant_0;						
 		} else if (Input.GetKeyDown (KeyCode.L)) {
-			myState = States.papier_chavant;
+			myState = States.chavant_1;
 		}			
 	}
 	
-	void papier_chavant() {
+	void chavant_1() {
 		
 		text.text = "'On peut parfois y rencontrer un champion de Molkky.' Voilà tout ce qui est écrit sur ce papier.\n\n" +
 					"Appui sur N pour aller vers le Nord, sur E pour aller vers l'Est ou sur O pour aller vers l'Ouest.";
@@ -100,13 +106,26 @@ public class TextController : MonoBehaviour
 	
 	}
 	
+	void chavant_2() {
+		text.text = "Bon c'est toujours le meme arret de tram finalement.\n\n" +
+					"Appui sur N pour aller vers le Nord, sur E pour aller vers l'Est ou sur O pour aller vers l'Ouest.";
+					
+		if (Input.GetKeyDown (KeyCode.N)) {			
+			myState = States.nord;						
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			myState = States.est;
+		} else if (Input.GetKeyDown (KeyCode.O)) {
+			myState = States.ouest;
+		}
+	}
+	
 	void nord() {
 		
 		text.text = "Te voilà sur les berges de l'Isère. C'est joli, mais il peut probable de trouver un joueur de Molkky ici, non ?\n\n" + 
 					"Appui sur C pour retourner à Chavant.";
 					
 		if (Input.GetKeyDown (KeyCode.C)) {
-			myState = States.papier_chavant;
+			myState = States.chavant_2;
 		}	
 	}
 	
@@ -116,13 +135,40 @@ public class TextController : MonoBehaviour
 					"Appui sur C pour retourner à Chavant.";
 			
 		if (Input.GetKeyDown (KeyCode.C)) {
-			myState = States.papier_chavant;
+			myState = States.chavant_2;
 		}
 	}
 	
 	void est() {
 		
+		text.text = "Le parc Mistral, effectivement haut lieu du Molkky Grenoblois. Mais soudain une goutte tombe, puis une autre, et une autre. " +
+					"Bientot c'est un vrai déluge. Ca ne te rappelle rien ? Peut-etre que tu peux te mettre à l'abris quelque part ?\n\n" + 
+					"Appui sur B pour t'approcher de la Bobine, sur C pour retourner à Chavant.";
 		
+		if (Input.GetKeyDown (KeyCode.C)) {
+			myState = States.chavant_3;
+		} else if (Input.GetKeyDown (KeyCode.B)) {
+			myState = States.bobine;
+		}
+		
+	}
+	
+	void chavant_3() {
+	
+		text.text = "Il pleut et les abris sont bondés... Mauvaise idée...\n\n" +
+					"Appui sur R pour retourner vers le Parc Mistral.";
+		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			myState = States.est;
+		}
+		
+	}
+	
+	void bobine() {
+	
+		text.text = "La terrasse couverte est pleine et il n'y a pas de chaises, l'intérieur n'est pas mieux.\n" + 
+					"\n\n" +
+					"Appui sur R pour retourner vers le Parc Mistral.";
 		
 	}
 	
